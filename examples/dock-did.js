76c6da82db7613ee15c74bec592b09b0936e9a19
Mike Parkhill <mike@dock.io>
2021-10-08 18:15:09 -0400

    fleshed out DID actions

--- examples/dock-did.js
+++ examples/dock-did.js
@@ -6,4 +6,34 @@ async function createDID(data) {
   return result.data;
 }
 
-module.exports = createDID;
+async function getDID(id) {
+  const result = await http.get(`dids/${id}`);
+  console.log(`Response: ${JSON.stringify(result.data)}`);
+  return result.data;
+}
+
+async function listDIDs() {
+  const result = await http.get('dids/');
+  console.log(`Response: ${JSON.stringify(result.data)}`);
+  return result.data;
+}
+
+async function updateDID(did, body) {
+  const result = await http.patch(`dids/${did}`, body);
+  console.log(`Response: ${JSON.stringify(result.data)}`);
+  return result.data;
+}
+
+async function deleteDID(did) {
+  const result = await http.callDelete(`dids/${did}`);
+  console.log(`Response: ${JSON.stringify(result.data)}`);
+  return result.data;
+}
+
+module.exports = {
+  createDID,
+  getDID,
+  listDIDs,
+  updateDID,
+  deleteDID,
+};
--- utils/http-utils.js
+++ utils/http-utils.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const baseUrl = process.env.API_URL;
 const axiosHeaders = {
@@ -17,8 +20,45 @@ async function post(relativeUrl, data) {
   } catch (err) {
     console.error(`Failed: ${err}`);
     return {};
-    // throw err;
   }
 }
 
-module.exports = { post };
+async function patch(relativeUrl, data) {
+  try {
+    const fullUrl = `${baseUrl}/${relativeUrl}`;
+    console.log(`Patching request to ${fullUrl}`);
+
+    return await axios.patch(fullUrl, data, axiosHeaders);
+  } catch (err) {
+    console.error(`Failed: ${err}`);
+    return {};
+  }
+}
+
+async function callDelete(relativeUrl) {
+  try {
+    const fullUrl = `${baseUrl}/${relativeUrl}`;
+    console.log(`Deleting ${fullUrl}`);
+
+    return await axios.delete(fullUrl, axiosHeaders);
+  } catch (err) {
+    console.error(`Failed: ${err}`);
+    return {};
+  }
+}
+
+async function get(relativeUrl) {
+  try {
+    const fullUrl = `${baseUrl}/${relativeUrl}`;
+    console.log(`Getting data from ${fullUrl}`);
+
+    return await axios.get(fullUrl, axiosHeaders);
+  } catch (err) {
+    console.error(`Failed: ${err}`);
+    return {};
+  }
+}
+
+module.exports = {
+  post, get, patch, callDelete,
+};
