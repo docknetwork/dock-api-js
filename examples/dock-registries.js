d408c429bf23a0547aae1138a042aad6eae6388f
Mike Parkhill <mike@dock.io>
2021-10-18 17:36:43 -0400

    added unrevoke example

--- examples/dock-registries.js
+++ examples/dock-registries.js
@@ -11,6 +11,34 @@ async function createRegistry(policyDid) {
   return http.sendAndLog(() => http.post('registries/', data));
 }
 
+async function revoke(registryId, credential) {
+  const url = `registries/${registryId}`;
+
+  const data = {
+    action: 'revoke',
+    credentialIds: [
+      credential.id,
+    ],
+  };
+
+  return http.sendAndLog(() => http.post(url, data));
+}
+
+async function unrevoke(registryId, credential) {
+  const url = `registries/${registryId}`;
+
+  const data = {
+    action: 'unrevoke',
+    credentialIds: [
+      credential.id,
+    ],
+  };
+
+  return http.sendAndLog(() => http.post(url, data));
+}
+
 module.exports = {
   createRegistry,
+  revoke,
+  unrevoke,
 };
